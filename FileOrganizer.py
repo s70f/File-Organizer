78bfedc5131b67b9a15b4e55b56bf311c7643bbf
s70f <suhaybsana@gmail.com>
2021-09-28 20:34:22 -0400

    created a sort function for extensions

--- FileOrganizer.py
+++ FileOrganizer.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 
 # Append all files in the downloads directory to a list
 USERNAME = os.getlogin()
@@ -6,3 +7,11 @@ USERNAME = os.getlogin()
 files = [file for file in os.listdir(
     r'C:\Users\{0}\Desktop\To-Do'.format(USERNAME))]
 print(files)
+
+# sorting by extension function
+
+
+def sort_by_extension(file: str, target_dir: str, *extensions: tuple):
+    if file.endswith(extensions):
+        shutil.move(r'C:\Users\{0}\Desktop\To-Do\{1}'.format(USERNAME,
+                    file), target_dir)
